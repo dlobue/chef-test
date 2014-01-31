85ac3cde5a9fa3638656e04aa8ea434205e98a48
Dominic LoBue <dominic.lobue@gmail.com>
2014-01-31 08:23:08 -0800

    i wanna see

--- cookbooks/dumb/recipes/default.rb
+++ cookbooks/dumb/recipes/default.rb
@@ -4,5 +4,5 @@ log "THIS IS A DUMB LOG LINE. I SAY:     ----      #{node.dumb.content}"
 log "THIS IS ME! ---- #{node.persist.to_indexed_json}\n\n\n\n"
 
 
-log "THESE ARE MY FRIENDS!! ----------- #{short_search(:recipes => "dumb::default")}"
+log "THESE ARE MY FRIENDS!! ----------- #{short_search(:recipes => "dumb::default").results}"
 
