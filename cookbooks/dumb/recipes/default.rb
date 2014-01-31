696c8e9d10f088de294e6ef6d2dc60f3e152fe2f
Dominic LoBue <dominic.lobue@gmail.com>
2014-01-31 08:19:00 -0800

    changes

--- cookbooks/dumb/recipes/default.rb
+++ cookbooks/dumb/recipes/default.rb
@@ -1,6 +1,8 @@
 
 log "THIS IS A DUMB LOG LINE. I SAY:     ----      #{node.dumb.content}"
 
-log "THIS IS ME! ---- #{node.persist.to_json}"
+log "THIS IS ME! ---- #{node.persist.to_indexed_json}\n\n\n\n"
 
+
+log "THESE ARE MY FRIENDS!! ----------- #{short_search(:recipes => "dumb::default")}"
 
